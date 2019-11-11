ad4b8038753b9f5c9118deb4c72f121b97585158
Danny <dannybcreative@gmail.com>
2019-11-11 09:47:41 -0800

    animation updated, art approved, project complete

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
 /* Menu Background & Scroll Top */
-const navWrapper = document.querySelector('.nav-wrapper'),
-  navBar = document.querySelector('.navbar-collapse'),
-  logoBox = document.querySelector('.logo-box'),
-  navLogo = document.querySelector('.nav-logo');
+// const navWrapper = document.querySelector('.nav-wrapper'),
+//   navBar = document.querySelector('.navbar-collapse'),
+//   logoBox = document.querySelector('.logo-box'),
+//   navLogo = document.querySelector('.nav-logo');
 
-window.addEventListener('resize', () => {
-  if (window.innerWidth < 992) {
-    navLogo.classList.remove('show');
-    navBar.classList.remove('show');
-    logoBox.classList.remove('show');
-  } else if (window.innerWidth >= 992 && window.scrollY === 0) {
-    navWrapper.classList.remove('show');
-    navLogo.classList.remove('show');
+// window.addEventListener('resize', () => {
+//   if (window.innerWidth < 992) {
+//     navLogo.classList.remove('show');
+//     navBar.classList.remove('show');
+//     logoBox.classList.remove('show');
+//   } else if (window.innerWidth >= 992 && window.scrollY === 0) {
+//     navWrapper.classList.remove('show');
+//     navLogo.classList.remove('show');
 
-  }
-});
+//   }
+// });
 
-window.addEventListener('scroll', () => {
-  if (window.innerWidth >= 992 && window.scrollY > 50) {
-    navWrapper.classList.add('show');
-    navBar.classList.add('show');
-    logoBox.classList.add('show');
-    navLogo.classList.add('show');
-  } else if (window.innerWidth >= 992 && window.scrollY < 100) {
-    navWrapper.classList.remove('show');
-    navBar.classList.remove('show');
-    logoBox.classList.remove('show');
-    navLogo.classList.remove('show');
-  }
-});
+// window.addEventListener('scroll', () => {
+//   if (window.innerWidth >= 992 && window.scrollY > 50) {
+//     navWrapper.classList.add('show');
+//     navBar.classList.add('show');
+//     logoBox.classList.add('show');
+//     navLogo.classList.add('show');
+//   } else if (window.innerWidth >= 992 && window.scrollY < 100) {
+//     navWrapper.classList.remove('show');
+//     navBar.classList.remove('show');
+//     logoBox.classList.remove('show');
+//     navLogo.classList.remove('show');
+//   }
+// });
 
 /* Animation */
 function animateCSS(element, animationName) {
@@ -38,7 +38,7 @@ function animateCSS(element, animationName) {
   /* only on scroll */
   window.addEventListener('scroll', () => {
 
-    if (node.getBoundingClientRect().top < window.innerHeight / 1.5) {
+    if (node.getBoundingClientRect().top < window.innerHeight / 1.3) {
       node.classList.add('animated', animationName);
     }
 
@@ -48,12 +48,15 @@ function animateCSS(element, animationName) {
 }
 
 /* Animation Trigger */
-animateCSS('.about-us', 'fadeInUp');
+animateCSS('.information', 'fadeInUp');
+// animateCSS('.about-us', 'fadeInUp');
 animateCSS('#laser-info', 'fadeInRight');
-animateCSS('#botox', 'fadeInLeft');
-animateCSS('#fillers', 'fadeInRight');
-animateCSS('.sculpsure', 'fadeInUp');
-animateCSS('#hair-img', 'fadeIn');
-animateCSS('#hair-info', 'fadeIn');
-animateCSS('.chem-peels', 'fadeInUp');
-animateCSS('.consultation', 'fadeIn');
+animateCSS('#functional-info', 'fadeInLeft');
+animateCSS('.functional-health', 'fadeIn');
+animateCSS('#chiro-img', 'fadeInLeft');
+animateCSS('#chiro-info', 'fadeInRight');
+animateCSS('#aesthetics-info', 'fadeInLeft');
+animateCSS('#aesthetics-img', 'fadeInRight');
+animateCSS('.visit', 'fadeInUp');
+animateCSS('#mas-therapy', 'fadeInUp');
+animateCSS('.footer-1', 'fadeInUp');
